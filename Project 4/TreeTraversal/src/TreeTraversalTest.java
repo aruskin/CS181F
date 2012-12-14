10256ac263b70663ed9177dd2b324be8a2cb2f22
aruskin <asruskin@gmail.com>
2012-12-13 19:00:42 -0800

    first test passed with two cases

--- Project 4/TreeTraversal/src/TreeTraversalTest.java
+++ Project 4/TreeTraversal/src/TreeTraversalTest.java
@@ -8,10 +8,11 @@ import org.junit.Test;
 public class TreeTraversalTest {
 
 	@Test
+	//Converting (correctly formatted) text input to binary tree
 	public void testFileToTree() {
 		TreeTraversal tester = new TreeTraversal();
 		
-		//build tree to test equality against
+		//build simple tree to test equality against
 		BinaryTree<KeyTriple> testTree1 = new BinaryTree(new KeyTriple("First question?",
 											"left", "leftimg.gif",
 											"right", "rightimg.gif"), 
@@ -20,6 +21,31 @@ public class TreeTraversalTest {
 		
 		//toString makes sure that we're just testing to see if the trees contain the
 		//same values, rather than that the trees are the same object
-		assertEquals(testTree1.toString(), tester.fileToTree("src/test2.tree").toString());
+		assertEquals(testTree1.toString(), tester.fileToTree("src/test01.tree").toString());
+		
+		//build slightly more complicated tree to test equality against
+		BinaryTree<KeyTriple> right = new BinaryTree(new KeyTriple("Second right question?", 
+										"left 2r", "leftimg2r.gif",
+										"right 2r", "rightimg2r.gif"),
+										new BinaryTree(new KeyTriple("Answer D")),
+										new BinaryTree(new KeyTriple("Answer E")));
+		
+		BinaryTree<KeyTriple> lright =  new BinaryTree(new KeyTriple("Third lr question?", 
+											"left 3lr", "leftimg3lr.gif",
+											"right 3lr", "rightimg3lr.gif"),
+											new BinaryTree(new KeyTriple("Answer B")),
+											new BinaryTree(new KeyTriple("Answer C")));
+		
+		BinaryTree<KeyTriple> left = new BinaryTree(new KeyTriple("Second left question?", 
+										"left 2l", "leftimg2l.gif",
+										"right 2l", "rightimg2l.gif"),
+										new BinaryTree(new KeyTriple("Answer A")),
+										lright);
+		BinaryTree<KeyTriple> testTree2 = new BinaryTree(new KeyTriple("First question?",
+											"left 1", "leftimg1.gif", 
+											"right 1", "rightimg1.gif"),
+											left, right);
+		
+		assertEquals(testTree2.toString(), tester.fileToTree("src/test02.tree").toString());
 	}
 }
